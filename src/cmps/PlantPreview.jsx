3755087f583b9c2fe280148be56ba2a4a547ed11
Yonatan Erez <jontaner12@gmail.com>
2022-07-21 10:17:16 +0300

    preview

--- src/cmps/PlantPreview.jsx
+++ src/cmps/PlantPreview.jsx
@@ -5,10 +5,12 @@ export function PlantPreview({ plant, onRemovePlant }) {
     <section className="plant-preview">
       <Link to={`/plant/${plant._id}`}>
         <h2>{plant.name}</h2>
+        <img className="preview-img" src={plant.img}></img>
       </Link>
-      <img className="preview-img" src={plant.img}></img>
       <section className="below-image flex space-between">
-        <button className="remove-btn" onClick={() => onRemovePlant(plant._id)}><img src='https://res.cloudinary.com/ddhuvtrpp/image/upload/v1657709534/JungPlants%20Project/icons8-remove-50_i7jxar.png'/></button>
+        <button className="remove-btn" onClick={() => onRemovePlant(plant._id)}>
+          <img src="https://res.cloudinary.com/ddhuvtrpp/image/upload/v1657709534/JungPlants%20Project/icons8-remove-50_i7jxar.png" />
+        </button>
         <h4>{plant.price}$</h4>
       </section>
     </section>
